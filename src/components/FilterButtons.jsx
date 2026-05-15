5f0657c91e058506b5f84571ded8d0643d8f2d6e
Shantelwi <williamsshantel35@yahoo.com>
2026-05-14 19:12:30 -0700

    finished assignment

---
+++ src/components/FilterButtons.jsx
@@ -0,0 +1,11 @@
+function FilterButtons({filter, setFilter}) {
+    return (
+        <div>
+            <button onClick={() => setFilter('all')}>All</button>
+            <button onClick={() => setFilter('completed')}>Completed</button>
+            <button onClick={() => setFilter('pending')}>Pending</button>
+            <p>Current filter: {filter}</p>
+        </div>
+    )
+}
+export default FilterButtons;
--- src/exercises/lesson-04/BugEventPropagation.jsx
+++ src/exercises/lesson-04/BugEventPropagation.jsx
@@ -25,3 +25,5 @@ export default function BugEventPropagation() {
     </>
   );
 }
+
+// added preventDefault(); and stopPropagation(); events 
