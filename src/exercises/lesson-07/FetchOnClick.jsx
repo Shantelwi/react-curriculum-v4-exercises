ad49ffb3fee11e44396b0625211894e768743817
Shantel Williams <williamsshantel73@gmail.com>
2026-05-21 19:39:32 +0000

    finished assignment

--- src/exercises/lesson-07/FetchOnClick.jsx
+++ src/exercises/lesson-07/FetchOnClick.jsx
@@ -1,13 +1,40 @@
+import { useState } from 'react';
+import { getSinglePost } from './api';
 import './Lesson07Styles.css';
 
 export default function FetchOnClick() {
+  const [post, setPost] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  async function handleClick() {
+    try {
+      setLoading(true);
+      setError('');
+      const data = await getSinglePost(1);
+      setPost(data);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <div className="root">
       <h1 className="heading">Fetch single post on click</h1>
-      <button type="button">Get post</button>
+      <button type="button" onClick={handleClick}>
+        Get post
+      </button>
       <div className="content">
-        TODO: Replace me with fetched data when the <code>Get post</code> button
-        is clicked
+        {loading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+        {post && (
+          <>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </>
+        )}
       </div>
     </div>
   );
--- src/exercises/lesson-07/FetchOnRender.jsx
+++ src/exercises/lesson-07/FetchOnRender.jsx
@@ -1,11 +1,41 @@
+import { useEffect, useState } from 'react';
+import { getPosts } from './api';
 import './Lesson07Styles.css';
 
 export default function FetchOnRender() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    async function loadPosts() {
+      try {
+        setLoading(true);
+        setError('');
+
+        const data = await getPosts();
+        setPosts(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadPosts();
+  }, []);
+
   return (
     <div className="root">
       <h1 className="heading">Fetch list of posts on render</h1>
       <div className="content">
-        TODO: Replace me with fetched data when the component renders
+        {loading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+        {posts.map((post) => {
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>;
+        })}
       </div>
     </div>
   );
